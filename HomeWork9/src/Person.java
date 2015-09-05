647d310df7016a443a8ac3d6a46f1b40dfae4ea0
Ico <Ico@homme.mshome.net>
2015-09-05 14:07:37 +0300

    Add property

--- HomeWork9/src/Person.java
+++ HomeWork9/src/Person.java
@@ -1,4 +1,31 @@
+public class Person implements Comparable<Person> {
+	private String firstName;
+	private String lastName;
 
-public class Person {
+	public Person(String first, String last) {
+		setFirstName(first);
+		setLastName(last);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public int compareTo(Person o) {
+		return this.lastName.compareTo(o.lastName);
+	}
 
 }
