4bbcec395df5fa0ffc979abf560b5e134044cb22
Ico <Ico@homme.mshome.net>
2015-09-06 14:20:53 +0300

    Add add method from everyone structure

--- HomeWork9/src/problem3/Store.java
+++ HomeWork9/src/problem3/Store.java
@@ -1,27 +1,28 @@
 package problem3;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-
 import java.util.TreeMap;
 
-public class Store<T> {
-	private HashSet<Products> listOfProductsByTitle;
+public class Store {
+	private TreeMap<String, Products> listOfProductsByTitle;
 	// convert double price to string value to use like a key
 	private TreeMap<String, List<Products>> listOfProductsByPrice;
 	private TreeMap<String, TreeMap<String, List<Products>>> listOfProductsBySupplier;
 
 	public Store() {
-		setListOfProductsByTitle(new HashSet<Products>());
+		setListOfProductsByTitle(new TreeMap<String, Products>());
 		setListOfProductsByPrice(new TreeMap<String, List<Products>>());
 		setListOfProductsBySupplier(new TreeMap<String, TreeMap<String, List<Products>>>());
 	}
 
-	public HashSet<Products> getListOfProductsByTitle() {
+	public TreeMap<String, Products> getListOfProductsByTitle() {
 		return listOfProductsByTitle;
 	}
 
-	public void setListOfProductsByTitle(HashSet<Products> listOfProductsByTitle) {
+	public void setListOfProductsByTitle(
+			TreeMap<String, Products> listOfProductsByTitle) {
 		this.listOfProductsByTitle = listOfProductsByTitle;
 	}
 
@@ -43,8 +44,28 @@ public class Store<T> {
 		this.listOfProductsBySupplier = listOfProductsBySupplier;
 	}
 
-	public void add() {
-		// TODO
+	private void addByTitle(String title, String supplier, double price, int id) {
+		Products product = new Products(title, supplier, price, id);
+		getListOfProductsByTitle().put(title, product);
+	}
+
+	private void addByPrice(String title, String supplier, double price, int id) {
+		Products product = new Products(title, supplier, price, id);
+		String priceValue = Double.toString(price);
+		if (!getListOfProductsByPrice().containsKey(priceValue)) {
+			List<Products> listOfProducts = new ArrayList<Products>();
+			listOfProducts.add(product);
+			getListOfProductsByPrice().put(priceValue, listOfProducts);
+		} else {
+			List<Products> listOfProducts = getListOfProductsByPrice().get(
+					priceValue);
+			listOfProducts.add(product);
+		}
+	}
+
+	public void add(String title, String supplier, double price, int id) {
+		addByTitle(title, supplier, price, id);
+		addByPrice(title, supplier, price, id);
 	}
 
 	public boolean remove() {
@@ -52,7 +73,7 @@ public class Store<T> {
 		return false;
 	}
 
-	public List<T> findPriceRange() {
+	public List<Products> findPriceRange() {
 		// TODO
 		return null;
 
---
+++ HomeWork9/src/problem3/StoreDemo.java
@@ -0,0 +1,11 @@
+package problem3;
+
+import java.util.HashSet;
+
+public class StoreDemo {
+	public static void main(String[] args) {
+		Store store = new Store();
+
+	}
+
+}
