f4595c3f1af177e74b3a34f3acb10f907eaa801e
Ico <Ico@homme.mshome.net>
2015-09-06 12:15:38 +0300

    Add remove method

--- HomeWork9/src/problem2/BiDictionary.java
+++ HomeWork9/src/problem2/BiDictionary.java
@@ -1,18 +1,21 @@
 package problem2;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class BiDictionary<K1, K2, T> {
 	private Map<K1, List<T>> valuesByFirstKey;
 	private Map<K2, List<T>> valuesBySecondKey;
+	private Map<String, List<T>> valuesByBothKeys;
+	private static final String KEY_SEPARATOR = "_";
 
 	public BiDictionary() {
 		setValuesByFirstKey(new TreeMap<K1, List<T>>());
 		setValuesBySecondKey(new TreeMap<K2, List<T>>());
+		setValuesByBothKeys(new TreeMap<String, List<T>>());
 	}
 
 	public Map<K1, List<T>> getValuesByFirstKey() {
@@ -29,6 +32,14 @@ public class BiDictionary<K1, K2, T> {
 
 	public void setValuesBySecondKey(Map<K2, List<T>> valuesBySecondKey) {
 		this.valuesBySecondKey = valuesBySecondKey;
+	}
+
+	public Map<String, List<T>> getValuesByBothKeys() {
+		return valuesByBothKeys;
+	}
+
+	public void setValuesByBothKeys(Map<String, List<T>> valuesByBothKeys) {
+		this.valuesByBothKeys = valuesByBothKeys;
 	}
 
 	public void add(K1 key1, K2 key2, T value) {
@@ -48,26 +59,75 @@ public class BiDictionary<K1, K2, T> {
 			List<T> existingList = getValuesBySecondKey().get(key2);
 			existingList.add(value);
 		}
+		String k3 = key1 + KEY_SEPARATOR + key2;
+		if (!getValuesByBothKeys().containsKey(k3)) {
+			List<T> list3 = new ArrayList<T>();
+			list3.add(value);
+			getValuesByBothKeys().put(k3, list3);
+		} else {
+			List<T> existingList = getValuesByBothKeys().get(k3);
+			existingList.add(value);
+		}
 	}
 
-	public List<T> find(K1 key1, K2 key2) {
-		// TODO
-		return null;
+	public TreeSet<T> find(K1 key1, K2 key2) {
+		TreeSet<T> resultList = new TreeSet<T>();
+		String key3 = key1 + "_" + key2;
+		if (getValuesByBothKeys().containsKey(key3)) {
+			resultList.addAll(getValuesByBothKeys().get(key3));
+		}
+		if (resultList.size() > 0) {
+			return resultList;
+		} else {
+			return null;
+		}
 	}
 
 	public List<T> findByKey1(K1 key1) {
-		// TODO
-		return null;
+		List<T> resultList = new ArrayList<T>();
+		if (getValuesByFirstKey().containsKey(key1)) {
+			resultList.addAll(getValuesByFirstKey().get(key1));
+		}
+		if (resultList.size() > 0) {
+			return resultList;
+		} else {
+			return null;
+		}
 	}
 
 	public List<T> findByKey2(K2 key2) {
-		// TODO
-		return null;
+		List<T> resultList = new ArrayList<T>();
+		if (getValuesBySecondKey().containsKey(key2)) {
+			resultList.addAll(getValuesBySecondKey().get(key2));
+		}
+		if (resultList.size() > 0) {
+			return resultList;
+		} else {
+			return null;
+		}
 	}
 
 	public boolean remove(K1 key1, K2 key2) {
-		// TODO
+		String key3 = key1 + KEY_SEPARATOR + key2;
+		List<T> listMatching = new ArrayList<T>();
+		// first we remove bothKey and get the element which matching
+		if (getValuesByBothKeys().containsKey(key3)) {
+			listMatching = getValuesByBothKeys().remove(key3);
+			System.out.println("Remove from Both key list");
+		}
+		// then we compares with every single key and remove only matching
+		if (getValuesByFirstKey().containsKey(key1)) {
+			for (T elementToRemov : listMatching) {
+				getValuesByFirstKey().get(key1).remove(elementToRemov);
+			}
+			System.out.println("Remove from first key list");
+		}
+		if (getValuesBySecondKey().containsKey(key2)) {
+			for (T elementToRemov : listMatching) {
+				getValuesBySecondKey().get(key2).remove(elementToRemov);
+			}
+			System.out.println("Remove from second key list");
+		}
 		return false;
 	}
-
 }
--- HomeWork9/src/problem2/BiDictionaryTest.java
+++ HomeWork9/src/problem2/BiDictionaryTest.java
@@ -1,5 +1,11 @@
 package problem2;
 
+import java.util.*;
+
+import javax.swing.plaf.ListUI;
+
+import java.lang.Object;
+
 public class BiDictionaryTest {
 	public static void main(String[] args) {
 		BiDictionary<String, String, Integer> distances = new BiDictionary<String, String, Integer>();
@@ -10,6 +16,17 @@ public class BiDictionaryTest {
 		distances.add("Sofia", "Bourgas", 383);
 		distances.add("Plovdiv", "Bourgas", 253);
 		distances.add("Plovdiv", "Bourgas", 292);
+
+		Set<Integer> a = distances.find("Sofia", "Varna");
+		for (Integer i : a) {
+			System.out.println(i);
+		}
+		System.out.println("-----------");
+		distances.remove("Sofia", "Varna");
+		List<Integer> b = distances.findByKey1("Sofia");
+		for (Integer i : b) {
+			System.out.println(i);
+		}
 
 	}
 }
